4c973bf72a1ed779645373ff0170316a93f9e5c1
sean00880 <theelitereapersanctum@gmail.com>
2023-01-04 14:49:27 -0500

    changes

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Navbar from "../components/Layouts/Navbar";
+import MainBanner from "../components/HomePages/DigitalMarketing/MainBanner";
+import Partner from "../components/HomePages/DigitalMarketing/Partner";
+import MarketingProcess from "../components/HomePages/DigitalMarketing/MarketingProcess";
+import AboutContent from "../components/HomePages/DigitalMarketing/AboutContent";
+import OurServices from "../components/HomePages/DigitalMarketing/OurServices";
+import FunFacts from "../components/HomePages/DigitalMarketing/FunFacts";
+import Testimonials from "../components/HomePages/DigitalMarketing/Testimonials";
+import PricingPlans from "../components/HomePages/DigitalMarketing/PricingPlans";
+import TeamMember from "../components/HomePages/DigitalMarketing/TeamMember";
+import LatestNews from "../components/HomePages/DigitalMarketing/LatestNews";
+import CtaArea from "../components/Common/CtaArea";
+import Newsletter from "../components/Common/Newsletter";
+import Footer from "../components/Layouts/Footer";
+
+const DigitalMarketing = () => {
+  return (
+    <>
+      <Navbar />
+
+      <MainBanner />
+      
+      <Partner />
+
+      <MarketingProcess />
+
+      <AboutContent />
+
+      <OurServices />
+
+      <FunFacts />
+
+      <Testimonials />
+
+      <PricingPlans />
+
+      <CtaArea />
+
+      <TeamMember />
+
+      <LatestNews />
+
+ 
+
+      <div className="pb-100">
+        <Newsletter />
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default DigitalMarketing;
